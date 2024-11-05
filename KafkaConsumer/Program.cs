bd53b2661e7952aa281716559b079086e19dbe2c
Lowbeat <sergeyafanasievv@gmail.com>
2024-11-05 16:32:11 +0300

    Add files via upload

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using Confluent.Kafka;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Конфигурация Consumer
+        var consumerConfig = new ConsumerConfig
+        {
+            GroupId = "order-consumer-group", // Укажите группу потребителей
+            BootstrapServers = "localhost:9092", // Адрес вашего Kafka брокера
+            AutoOffsetReset = AutoOffsetReset.Earliest, // Начинаем с самого раннего сообщения
+            EnableAutoCommit = false // Выключаем автоматическую фиксацию оффсетов
+        };
+
+        // Создание Consumer для чтения сообщений
+        using var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+        consumer.Subscribe("order-topic"); // Подписка на топик Kafka
+
+        Console.WriteLine("Waiting for messages...");
+
+        var seenOffsets = new HashSet<long>(); // Следим за хешами для гарантии
+
+        // Бесконечный цикл чтения сообщений
+        while (true)
+        {
+            try
+            {
+                var consumeResult = consumer.Consume(CancellationToken.None);
+
+                if (!seenOffsets.Contains(consumeResult.TopicPartitionOffset.GetHashCode()))
+                {
+                    // Вывод сообщения и добавление оффсета в seen_offsets
+                    Console.WriteLine($"Received message: {consumeResult.Message.Value} at {consumeResult.TopicPartitionOffset}");
+                    seenOffsets.Add(consumeResult.TopicPartitionOffset.GetHashCode());
+                }
+            }
+            catch (ConsumeException e)
+            {
+                // Обработка ошибки потребления сообщения
+                Console.WriteLine($"Error occurred: {e.Error.Reason}");
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,52 @@
+﻿using avro;
+using Avro.IO;
+using Avro.Specific;
+using Confluent.Kafka;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (var i = 1; ; i++)
+        {
+            // Создаем объект User
+            var user = new Order
+            {
+                orderId = i,
+                strings = new List<string>(new []{$"dish{i}", $"dish{i+1}"}),
+                date_ = (long)DateTime.Now.Subtract(DateTime.Now).TotalSeconds,
+                total_cost = i + i + 1
+            };
+
+            // Сериализация объекта User в байты с помощью Avro
+            byte[] avroData;
+            using (var ms = new MemoryStream())
+            {
+                var writer = new BinaryEncoder(ms);
+                var datumWriter = new SpecificDatumWriter<Order>(user.Schema);
+                datumWriter.Write(user, writer);
+                avroData = ms.ToArray();
+            }
+
+            // Конфигурация Kafka Producer
+            var producerConfig = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"  // Укажите адрес Kafka-брокера
+            };
+
+            // Отправка сообщения в Kafka
+            using (var producer = new ProducerBuilder<Null, byte[]>(producerConfig).Build())
+            {
+                var message = new Message<Null, byte[]>
+                {
+                    Value = avroData
+                };
+
+                var deliveryResult = producer.ProduceAsync("order-topic", message).GetAwaiter().GetResult();
+                Console.WriteLine($"Message delivered to {deliveryResult.TopicPartitionOffset}");
+            }
+            
+            Thread.Sleep(5000);
+        }
+    }
+}
